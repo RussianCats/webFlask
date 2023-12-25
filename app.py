40299e9f65b1bf95ec04f722946667d9c20ef806
RussianCats <bigfatrussiancat@gmail.com>
2023-12-25 21:26:46 +0300

    добавил регистрацию пользователей

--- app.py
+++ app.py
@@ -1,81 +1,68 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+import datetime
+import email
+import os
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-from userLogin import UserLogin
+
+# конфигурация
 
 app = Flask(__name__)
-
-# Настройка подключения к базе данных
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:5892@localhost:5432/web'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Инициализация SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:5892@localhost/web'
 db = SQLAlchemy(app)
 
-# Настройка Flask-Login
-login_manager = LoginManager()
-login_manager.init_app(app)
 
-# Данная функция заносит в сессию информацию о зарегистрированном пользователе, используя определенные в классе методы. 
-# После этого сессионная информация будет присутствовать во всех запросах к серверу. Для ее обработки во Flask-Login определен специальный декоратор: 
-@login_manager.user_loader
-def load_user(user_id):
-    print("load_user")
-    return UserLogin().fromDB(user_id, dbase)
 
-# Определение модели Role
-class Role(db.Model):
-    __tablename__ = 'roles'
+
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.Text, nullable=False)
-    label = db.Column(db.Text, nullable=False)
-
-    # Связь с моделью User
-    users = db.relationship('User', backref='role', lazy=True)
+    email = db.Column(db.String(50), unique=True)
+    psw = db.Column(db.String(500), nullable=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<Role {self.label}>'
+        return f"<users {self.id}>"
 
-# Определение модели User
-class User(db.Model):
-    __tablename__ = 'users'
+class Profiles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    login = db.Column(db.Text, nullable=False)
-    password = db.Column(db.Text, nullable=False)  
-    fio = db.Column(db.Text, nullable=False)
-    id_role = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
-    is_blocked = db.Column(db.Integer, default=0)
-
+    name = db.Column(db.String(50), nullable=True)
+    old = db.Column(db.Integer)
+    city = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     def __repr__(self):
-        return f'<User {self.login}>'
+        return f"<profiles {self.id}>"
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Главная")
 
-@app.route('/auth')
-def auth():
-    return render_template("auth.html")
+@app.route("/register", methods=("POST", "GET"))
+def register():
+    if request.method == "POST":
+        # здесь должна быть проверка корректности введенных данных
+        try:
+            # заносим данные пользователя в бд с кешом пароля
+            hash = generate_password_hash(request.form['psw'])
+            u = Users(email=request.form['email'], psw=hash)
+            db.session.add(u)
+            db.session.flush()
+            # добавляем данные в профайлес
+            p = Profiles(name=request.form['name'], old=request.form['old'],
+                         city=request.form['city'], user_id = u.id)
+            db.session.add(p)
+            db.session.commit() 
+            print(f"Пользователь зарегестрирован {request.form['email']}")
+        except:
+            # если ошибки, то откатываем состояние
+            db.session.rollback()
+            print("Ошибка записи пользователя в БД")
 
-@app.route('/reports')
-def reports():
-    return render_template("report.html")
-
-@app.route('/test')
-def test():
-    users = User.query.all()  # Извлечение всех пользователей из базы данных
-    return render_template("test.html", users=users)
+    
+    return render_template("register.html", title="Регистрация")
 
 
-# @app.route('/user/<string:name>/<int:id>')
-# def user(name, id):
-#     user = User.query.get(id)
-#     if user and user.name == name:
-#         return 'User page ' + str(name) + "-" + str(id)
-#     else:
-#         return 'User not found', 404
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
